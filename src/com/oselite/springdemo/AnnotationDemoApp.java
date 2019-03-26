4587914dbce65225bfe0ef433a36353a4004ae8d
OSelite <oselite.dev@gmail.com>
2019-03-26 12:50:53 +0100

    Created new coach

--- src/com/oselite/springdemo/AnnotationDemoApp.java
+++ src/com/oselite/springdemo/AnnotationDemoApp.java
@@ -5,17 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
-
-		//read spring config file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml ");
 		
-		//get the bean from spring container
+		//read the spring config
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//get the bean
+		Coach newCoach = context.getBean("footballCoach", Coach.class);
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
-		//call a method on the bean
+		//use beans
+		System.out.println(newCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyWorkout());
 		
-		//close the context
+		//close context
 		context.close();
 	}
 
---
+++ src/com/oselite/springdemo/FootballCoach.java
@@ -0,0 +1,13 @@
+package com.oselite.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Hit the ball!";
+	}
+
+}
